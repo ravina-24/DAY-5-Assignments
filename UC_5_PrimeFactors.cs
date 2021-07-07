9872f03268496fa819387515537c18cbd2198438
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-07 18:49:01 +0530

    [Ravina] Add:Finding the prime factors

---
+++ UC_5_PrimeFactors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Day_5_PrimeFactors
+{
+    class UC_5_PrimeFactors
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            int Num = 1;
+            int i = 1;
+
+            Console.WriteLine("Enter the Number ");
+            Num = int.Parse(Console.ReadLine());
+
+
+
+            for (i = 1; i <= Num; i++)
+            {
+
+                if ( Num % i == 0)
+                {
+                    Console.WriteLine("Factors are = " + i);
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
