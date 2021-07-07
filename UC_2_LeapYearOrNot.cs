93720c579cbf6854e72bd7af7d8ce1a245cbab76
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-07 18:35:14 +0530

    [Ravina] Add : Checking the year if the year is leap or not

---
+++ UC_2_LeapYearOrNot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeapYearOrNot
+{
+    class UC_2_LeapYearOrNot
+    {
+        static void Main(string[] args)
+        {
+
+            int year = 0;
+
+            Console.WriteLine("..............CHECKING the LEAP YEAR OR NOT............... ");
+
+            Console.WriteLine("Enter the year in four digit ");
+            year = int.Parse(Console.ReadLine());
+
+           if (( year % 100 == 0 ) && ( year % 400 == 0) && ( year % 4 == 0 ))
+           {
+                Console.WriteLine("Entered year is a LEAP YEAR");
+           }
+           else 
+           {
+                Console.WriteLine("Entered year is not a LEAP YEAR");
+           }
+
+            Console.ReadLine();
+        }
+    }
+}
