cac7b1dfcf2d51f78e8f47ec4b469b760890bd4a
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-07 19:11:39 +0530

    [Ravina] Add: Finding the largest among three numbers

---
+++ UC_10_FindTheLargestAmongThree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace UC_5_FindtheLargestAMongThree
+{
+    class UC_10_FindTheLargestAmongThree
+    {
+        static void Main(string[] args)
+        {
+
+            int Num1;
+            int Num2;
+            int Num3;
+
+            Console.WriteLine("Enter the Number 1 ");
+            Num1 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the Number 2 ");
+            Num2 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the Number 3");
+            Num3 = int.Parse(Console.ReadLine());
+
+            if ( Num1 > Num2  && Num1 > Num3 )
+            {
+                Console.WriteLine("Entered Number 1  is greatest ="  +Num1);
+
+            }
+            else if ( Num2 > Num1 && Num2 > Num3)
+            {
+                Console.WriteLine("Entered Number 2  is greatest= " + Num2);
+
+            }
+            else if ( Num3 > Num1 && Num3 > Num2 )
+            {
+                Console.WriteLine("Entered Number 3  is greatest = " + Num3);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
