82f429333ff811da5374f8e4aaa8202be6b457ca
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-07 19:03:15 +0530

    [Ravina] Add : Checking Whether the Number is odd or even

---
+++ UC_8_CheckTheOddOrEvenNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Day_5_NumberOddOREven
+{
+    class UC_8_CheckTheOddOrEvenNumber
+    {
+        static void Main(string[] args)
+        {
+            int Number = 1;
+
+
+            Console.WriteLine("Checking Whether the Number is odd or even ");
+
+            Console.WriteLine("Enter the Number");
+
+            Number = int.Parse(Console.ReadLine());
+
+            if ( Number%2 == 0)
+            {
+                Console.WriteLine("It is an even Number");
+            }
+            else
+            {
+                Console.WriteLine("It is an odd Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
