a6f2144f2a97ebf46e107e078d79dc9b92f8ca9e
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-07 18:39:07 +0530

    [Ravina] Add: Printing the Table for power of 2

---
+++ UC_3_PowerOfTwo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Day_5_PowerofTwo
+{
+    class UC_3_PowerOfTwo
+    {
+        static void Main(string[] args)
+        {
+
+            int Num = 0;
+            int i = 1;
+            Console.WriteLine("Enter the Number ");
+            Num = int.Parse(Console.ReadLine());
+            for (i = 1; i <= Num; i++)
+            {
+
+
+                double power =Math.Pow(2,i);
+                Console.WriteLine("Power of Two = " + power);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
