a8b8cf582f0c00b5a7f213b4908937324138b8da
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-07 18:52:50 +0530

    [Ravina] Add:Finding the Quotient and Remainder

---
+++ UC_6_QuotientAndRemainder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Day_5_QuotientAndRemainder
+{
+    class UC_6_QuotientAndRemainder
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int Num = 1;
+            int i = 1;
+            int Div = 1;
+            Console.WriteLine("Enter the Number");
+            Num = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the divider ");
+            Div = int.Parse(Console.ReadLine());
+
+            double Quotient = (Num / Div);
+            Console.WriteLine("Your Quotient is = " + Quotient);
+
+            double Remainder = (Num % Div );
+            Console.WriteLine("Your Remainder is = " + Remainder);
+
+        }
+
+
+                            
+    }
+}
