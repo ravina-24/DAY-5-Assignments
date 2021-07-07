90942dcc44faf39a0cec18a352548d67c59f4d2a
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-07 19:07:57 +0530

    [Ravina] Add : Checking whether the Alphabet is vowel or consonant

---
+++ UC_9_CheckTheVowelOrConsonant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Day_5_AlphabetVowelorConsonent
+{
+    class UC_9_CheckTheVowelOrConsonant
+    {
+        static void Main(string[] args)
+        {
+
+            char Alphabet;
+            
+
+            Console.WriteLine("Enter the Alphabet");
+            Alphabet = char.Parse(Console.ReadLine());
+
+            if ( Alphabet == 'a' || Alphabet == 'e'|| Alphabet == 'i' || Alphabet == 'o' || Alphabet == 'u' )  
+            { 
+                Console.WriteLine("Alphabet is a vowel");
+
+            }
+            else
+            {
+                Console.WriteLine("Alphabet is a consonant");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
